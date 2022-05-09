d770e71d242535a25b8f5da94994950ed0348830
Allison <amh03160@gmail.com>
2022-05-09 17:00:43 -0500

    yikes a whole mess

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from "react";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import './App.css';
+import AllAboutPlants from "./components/AllAboutPlants";
+import Navbar from "./components/Navbar"
+import NewUserForm from "./components/NewUserForm"
+import NewPlantForm from "./components/NewPlantForm"
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Link to="/all-about-plants">All About Plants</Link>
+      <switch>
+        <Route path="/" element={<AllAboutPlants />} />
+        <Route path="/navbar" element={<Navbar />} />
+        <Route path="/new-user-form" element={<NewUserForm />} />
+        <Route path="/new-plant-form" element={<NewPlantForm />} />
+      </switch>
+    </Router>
   );
 }
 
-export default App;
+
---
+++ client/src/components/NewUserForm.js
@@ -0,0 +1,7 @@
+function NewUserForm(){
+    return(
+        <h1>New User</h1>
+    )
+};
+
+export default NewUserForm;
