fc5f11de730c275d529846dbd741c4397fa00f9c
Allison <amh03160@gmail.com>
2022-05-09 10:53:38 -0500

    updated plant types columns

--- db/migrate/20220509151038_create_plant_types.rb
+++ db/migrate/20220509151038_create_plant_types.rb
@@ -2,10 +2,10 @@ class CreatePlantTypes < ActiveRecord::Migration[7.0]
   def change
     create_table :plant_types do |t|
       t.string :type
-      t.integer :water_needs
+      t.integer :water_frequency
+      t.integer :water_amount
       t.string :sun_needs
       t.string :type_examples
-
       t.timestamps
     end
   end
