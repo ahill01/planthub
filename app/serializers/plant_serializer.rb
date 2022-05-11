7128a3c981dc4057f7621c092acf3a549276b128
Allison <amh03160@gmail.com>
2022-05-11 16:34:06 -0500

    day3

--- app/serializers/plant_serializer.rb
+++ app/serializers/plant_serializer.rb
@@ -1,5 +1,11 @@
 class PlantSerializer < ActiveModel::Serializer
-  attributes :id, :name, :plant_type, :outside, :picture
+  include ActiveSupport::CoreExt::Numeric
+
+  attributes :id, :name, :plant_type, :outside, :picture, :last_watered
+
+  def last_watered
+    return self.object.waterings.first.to_fs(:long) 
+  end
 
   belongs_to :plant_category
   has_many :waterings
--- app/serializers/waterings_serializer.rb
+++ app/serializers/waterings_serializer.rb
@@ -1,5 +1,6 @@
 class WateringsSerializer < ActiveModel::Serializer
   attributes :id, :created_at
+  
   #custom method to formate date-time
   belongs_to :plant
 end
