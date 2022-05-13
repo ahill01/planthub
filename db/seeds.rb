15824c0280681170a8eb5b10d568aa46277be828
Allison <amh03160@gmail.com>
2022-05-13 12:57:43 -0500

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,6 +29,15 @@ Plant.create(name:"Susan",outside:false,plant_category_id:3,picture:"https://enc
 
 Plant.create(name:"Trinity",outside:false,plant_category_id:4,picture:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzGdg_w1li92i1NvvuDwGhqK93f28zWpMRBLr_m7GMH0QgXldNhQZ7dHJ2cGy--sGYBg0&usqp=CAU",plant_type:"waterlily",user_id:1)
 
+
+Plant.create(name:"Danny",outside:true,plant_category_id:3,picture:"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Leucanthemum_vulgare_%27Filigran%27_Flower_2200px.jpg/440px-Leucanthemum_vulgare_%27Filigran%27_Flower_2200px.jpg",plant_type:"Daisy",user_id:2)
+
+Plant.create(name:"Minty",outside:false,plant_category_id:1,picture:"https://assets.epicurious.com/photos/5787d8ff1edb4f17303e3ee1/4:3/w_1776,h_1332,c_limit/chocolate-mint-18072016.jpg",plant_type:"mint",user_id:2)
+
+Plant.create(name:"Minty Jr",outside:false,plant_category_id:1,picture:"https://assets.epicurious.com/photos/5787d8ff1edb4f17303e3ee1/4:3/w_1776,h_1332,c_limit/chocolate-mint-18072016.jpg",plant_type:"mint",user_id:2)
+
+Plant.create(name:"Basil",outside:false,plant_category_id:1,picture:"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/how-to-grow-basil-1590506465.jpg?crop=0.6666666666666666xw:1xh;center,top&resize=640",plant_type:"Basil",user_id:2)
+
 puts("Seeding Waterings")
 
 Watering.create(plant_id:1)
